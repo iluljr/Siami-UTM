e7349b8d0414058d31c31ca72db0db21cf36e258
iluljr <adhimfadlul@gmail.com>
2020-06-02 18:33:09 +0700

    update sql

--- siami.sql
+++ siami.sql
@@ -1,17 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
-<<<<<<< HEAD
--- Waktu pembuatan: 11 Bulan Mei 2020 pada 05.36
+-- Waktu pembuatan: 02 Jun 2020 pada 13.32
 -- Versi server: 10.4.8-MariaDB
 -- Versi PHP: 7.3.11
-=======
--- Waktu pembuatan: 30 Apr 2020 pada 12.01
--- Versi server: 10.1.38-MariaDB
--- Versi PHP: 7.3.4
->>>>>>> 77dda7c1e91d6a70445dd512f62a807b445ee188
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -36,14 +30,8 @@ USE `siami`;
 -- Struktur dari tabel `akun`
 --
 
-<<<<<<< HEAD
-DROP TABLE IF EXISTS `akun`;
-CREATE TABLE IF NOT EXISTS `akun` (
-  `id_user` int(11) NOT NULL AUTO_INCREMENT,
-=======
 CREATE TABLE `akun` (
   `id_user` int(11) NOT NULL,
->>>>>>> 77dda7c1e91d6a70445dd512f62a807b445ee188
   `username` varchar(60) NOT NULL,
   `password` varchar(60) NOT NULL,
   `level` int(11) NOT NULL
@@ -59,18 +47,14 @@ INSERT INTO `akun` (`id_user`, `username`, `password`, `level`) VALUES
 -- --------------------------------------------------------
 
 --
-<<<<<<< HEAD
 -- Struktur dari tabel `dosen`
 --
 
-DROP TABLE IF EXISTS `dosen`;
-CREATE TABLE IF NOT EXISTS `dosen` (
-  `id_dosen` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `dosen` (
+  `id_dosen` int(11) NOT NULL,
   `jumlah_dosen` int(11) NOT NULL,
-  `tahun_ajaran` int(11) NOT NULL,
-  PRIMARY KEY (`id_dosen`),
-  KEY `dosen_ibfk_1` (`tahun_ajaran`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+  `tahun_ajaran` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `dosen`
@@ -85,12 +69,10 @@ INSERT INTO `dosen` (`id_dosen`, `jumlah_dosen`, `tahun_ajaran`) VALUES
 -- Struktur dari tabel `fakultas`
 --
 
-DROP TABLE IF EXISTS `fakultas`;
-CREATE TABLE IF NOT EXISTS `fakultas` (
-  `id_fakultas` int(11) NOT NULL AUTO_INCREMENT,
-  `nama_fakultas` varchar(251) NOT NULL,
-  PRIMARY KEY (`id_fakultas`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `fakultas` (
+  `id_fakultas` int(11) NOT NULL,
+  `nama_fakultas` varchar(251) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `fakultas`
@@ -111,12 +93,10 @@ INSERT INTO `fakultas` (`id_fakultas`, `nama_fakultas`) VALUES
 -- Struktur dari tabel `jenjang`
 --
 
-DROP TABLE IF EXISTS `jenjang`;
-CREATE TABLE IF NOT EXISTS `jenjang` (
-  `id_jenjang` int(11) NOT NULL AUTO_INCREMENT,
-  `jenjang` varchar(10) NOT NULL,
-  PRIMARY KEY (`id_jenjang`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `jenjang` (
+  `id_jenjang` int(11) NOT NULL,
+  `jenjang` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `jenjang`
@@ -133,31 +113,17 @@ INSERT INTO `jenjang` (`id_jenjang`, `jenjang`) VALUES
 -- Struktur dari tabel `prodi`
 --
 
-DROP TABLE IF EXISTS `prodi`;
-CREATE TABLE IF NOT EXISTS `prodi` (
-  `id_prodi` int(11) NOT NULL AUTO_INCREMENT,
-  `nama_prodi` varchar(255) NOT NULL,
-  `id_fakultas` int(11) NOT NULL,
-  `id_jenjang` int(11) NOT NULL,
-  PRIMARY KEY (`id_prodi`),
-  KEY `id_fakultas` (`id_fakultas`),
-  KEY `id_jenjang` (`id_jenjang`)
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4;
-=======
--- Struktur dari tabel `prodi`
---
-
 CREATE TABLE `prodi` (
   `id_prodi` int(11) NOT NULL,
-  `nama_prodi` varchar(255) NOT NULL
+  `nama_prodi` varchar(255) NOT NULL,
+  `id_fakultas` int(11) NOT NULL,
+  `id_jenjang` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
->>>>>>> 77dda7c1e91d6a70445dd512f62a807b445ee188
 
 --
 -- Dumping data untuk tabel `prodi`
 --
 
-<<<<<<< HEAD
 INSERT INTO `prodi` (`id_prodi`, `nama_prodi`, `id_fakultas`, `id_jenjang`) VALUES
 (1, 'Teknik Informatika', 4, 2),
 (2, 'Teknik Elektro', 4, 2),
@@ -189,14 +155,6 @@ INSERT INTO `prodi` (`id_prodi`, `nama_prodi`, `id_fakultas`, `id_jenjang`) VALU
 (28, 'Ilmu Ekonomi', 2, 3),
 (29, 'Manajemen', 2, 3),
 (30, 'Akuntansi Forensik', 2, 3);
-=======
-INSERT INTO `prodi` (`id_prodi`, `nama_prodi`) VALUES
-(1, 'Teknik Informatika'),
-(2, 'Teknik Elektro'),
-(3, 'Teknik Industri'),
-(4, 'Teknik Mekatronika'),
-(5, 'Sistem Informasi');
->>>>>>> 77dda7c1e91d6a70445dd512f62a807b445ee188
 
 -- --------------------------------------------------------
 
@@ -204,41 +162,30 @@ INSERT INTO `prodi` (`id_prodi`, `nama_prodi`) VALUES
 -- Struktur dari tabel `tabel_2a`
 --
 
-<<<<<<< HEAD
-DROP TABLE IF EXISTS `tabel_2a`;
-CREATE TABLE IF NOT EXISTS `tabel_2a` (
-  `id_tabel2a` int(11) NOT NULL AUTO_INCREMENT,
-=======
 CREATE TABLE `tabel_2a` (
   `id_tabel2a` int(11) NOT NULL,
->>>>>>> 77dda7c1e91d6a70445dd512f62a807b445ee188
-  `id_tahun` int(11) NOT NULL,
+  `tahun` int(11) NOT NULL,
   `daya_tampung` int(11) NOT NULL,
   `pendaftar` int(11) NOT NULL,
   `lulus_seleksi` int(11) NOT NULL,
   `jmb_reguler` int(11) NOT NULL,
   `jmb_transfer` int(11) NOT NULL,
   `jma_reguler` int(11) NOT NULL,
-<<<<<<< HEAD
-  `jma_transfer` int(11) NOT NULL,
-  PRIMARY KEY (`id_tabel2a`),
-  KEY `tahun` (`id_tahun`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+  `jma_transfer` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `tabel_2a`
 --
 
-INSERT INTO `tabel_2a` (`id_tabel2a`, `id_tahun`, `daya_tampung`, `pendaftar`, `lulus_seleksi`, `jmb_reguler`, `jmb_transfer`, `jma_reguler`, `jma_transfer`) VALUES
-(1, 1, 149, 1130, 144, 114, 10, 434, 19),
-(2, 2, 174, 1529, 174, 147, 3, 584, 17),
-(3, 3, 139, 2081, 139, 126, 13, 585, 22),
-(4, 4, 240, 1960, 247, 187, 6, 631, 26),
-(5, 5, 120, 1403, 166, 141, 18, 645, 30);
-=======
-  `jma_transfer` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
->>>>>>> 77dda7c1e91d6a70445dd512f62a807b445ee188
+INSERT INTO `tabel_2a` (`id_tabel2a`, `tahun`, `daya_tampung`, `pendaftar`, `lulus_seleksi`, `jmb_reguler`, `jmb_transfer`, `jma_reguler`, `jma_transfer`) VALUES
+(1, 2016, 149, 1130, 144, 114, 10, 434, 19),
+(2, 2017, 174, 1529, 174, 147, 3, 584, 17),
+(3, 2018, 139, 2081, 139, 126, 13, 585, 22),
+(4, 2019, 240, 1960, 247, 187, 6, 631, 26),
+(5, 2020, 120, 1403, 166, 141, 141, 645, 30),
+(7, 2015, 654, 343, 235, 532, 235, 674, 45),
+(8, 2021, 550, 3123, 3131, 532, 532, 674, 46);
 
 -- --------------------------------------------------------
 
@@ -246,19 +193,10 @@ INSERT INTO `tabel_2a` (`id_tabel2a`, `id_tahun`, `daya_tampung`, `pendaftar`, `
 -- Struktur dari tabel `tahun_ajaran`
 --
 
-<<<<<<< HEAD
-DROP TABLE IF EXISTS `tahun_ajaran`;
-CREATE TABLE IF NOT EXISTS `tahun_ajaran` (
-  `id_tahun_ajaran` int(11) NOT NULL AUTO_INCREMENT,
-  `tahun` int(11) NOT NULL,
-  PRIMARY KEY (`id_tahun_ajaran`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
-=======
 CREATE TABLE `tahun_ajaran` (
   `id_tahun_ajaran` int(11) NOT NULL,
   `tahun` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
->>>>>>> 77dda7c1e91d6a70445dd512f62a807b445ee188
 
 --
 -- Dumping data untuk tabel `tahun_ajaran`
@@ -274,23 +212,6 @@ INSERT INTO `tahun_ajaran` (`id_tahun_ajaran`, `tahun`) VALUES
 (7, 2014);
 
 --
-<<<<<<< HEAD
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
---
-
---
--- Ketidakleluasaan untuk tabel `dosen`
---
-ALTER TABLE `dosen`
-  ADD CONSTRAINT `dosen_ibfk_1` FOREIGN KEY (`tahun_ajaran`) REFERENCES `tahun_ajaran` (`id_tahun_ajaran`);
-
---
--- Ketidakleluasaan untuk tabel `prodi`
---
-ALTER TABLE `prodi`
-  ADD CONSTRAINT `prodi_ibfk_1` FOREIGN KEY (`id_fakultas`) REFERENCES `fakultas` (`id_fakultas`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `prodi_ibfk_2` FOREIGN KEY (`id_jenjang`) REFERENCES `jenjang` (`id_jenjang`);
-=======
 -- Indexes for dumped tables
 --
 
@@ -301,17 +222,37 @@ ALTER TABLE `akun`
   ADD PRIMARY KEY (`id_user`);
 
 --
+-- Indeks untuk tabel `dosen`
+--
+ALTER TABLE `dosen`
+  ADD PRIMARY KEY (`id_dosen`),
+  ADD KEY `dosen_ibfk_1` (`tahun_ajaran`);
+
+--
+-- Indeks untuk tabel `fakultas`
+--
+ALTER TABLE `fakultas`
+  ADD PRIMARY KEY (`id_fakultas`);
+
+--
+-- Indeks untuk tabel `jenjang`
+--
+ALTER TABLE `jenjang`
+  ADD PRIMARY KEY (`id_jenjang`);
+
+--
 -- Indeks untuk tabel `prodi`
 --
 ALTER TABLE `prodi`
-  ADD PRIMARY KEY (`id_prodi`);
+  ADD PRIMARY KEY (`id_prodi`),
+  ADD KEY `id_fakultas` (`id_fakultas`),
+  ADD KEY `id_jenjang` (`id_jenjang`);
 
 --
 -- Indeks untuk tabel `tabel_2a`
 --
 ALTER TABLE `tabel_2a`
-  ADD PRIMARY KEY (`id_tabel2a`),
-  ADD KEY `tahun` (`id_tahun`);
+  ADD PRIMARY KEY (`id_tabel2a`);
 
 --
 -- Indeks untuk tabel `tahun_ajaran`
@@ -330,16 +271,34 @@ ALTER TABLE `akun`
   MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT untuk tabel `dosen`
+--
+ALTER TABLE `dosen`
+  MODIFY `id_dosen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `fakultas`
+--
+ALTER TABLE `fakultas`
+  MODIFY `id_fakultas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT untuk tabel `jenjang`
+--
+ALTER TABLE `jenjang`
+  MODIFY `id_jenjang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT untuk tabel `prodi`
 --
 ALTER TABLE `prodi`
-  MODIFY `id_prodi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_prodi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT untuk tabel `tabel_2a`
 --
 ALTER TABLE `tabel_2a`
-  MODIFY `id_tabel2a` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_tabel2a` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT untuk tabel `tahun_ajaran`
@@ -350,13 +309,19 @@ ALTER TABLE `tahun_ajaran`
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
->>>>>>> 77dda7c1e91d6a70445dd512f62a807b445ee188
 
 --
--- Ketidakleluasaan untuk tabel `tabel_2a`
+-- Ketidakleluasaan untuk tabel `dosen`
 --
-ALTER TABLE `tabel_2a`
-  ADD CONSTRAINT `tabel_2a_ibfk_1` FOREIGN KEY (`id_tahun`) REFERENCES `tahun_ajaran` (`id_tahun_ajaran`);
+ALTER TABLE `dosen`
+  ADD CONSTRAINT `dosen_ibfk_1` FOREIGN KEY (`tahun_ajaran`) REFERENCES `tahun_ajaran` (`id_tahun_ajaran`);
+
+--
+-- Ketidakleluasaan untuk tabel `prodi`
+--
+ALTER TABLE `prodi`
+  ADD CONSTRAINT `prodi_ibfk_1` FOREIGN KEY (`id_fakultas`) REFERENCES `fakultas` (`id_fakultas`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `prodi_ibfk_2` FOREIGN KEY (`id_jenjang`) REFERENCES `jenjang` (`id_jenjang`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
